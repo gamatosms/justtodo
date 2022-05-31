cb496d21b546716187a13346faf6269405c6fd9b
Gabriel Matos <gsilva505@hotmail.com>
2022-05-31 19:55:02 -0300

    Colocando botao de delete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let btnAdd = document.querySelector('.btn__add')
 let listTodo = document.querySelector('.list__todo')
 let warning = document.querySelector('.input__warning')
 
+
 btnAdd.addEventListener('click', function () {
     if (inputTask.value.length > 0) {
         createItem(inputTask.value)
@@ -14,7 +15,13 @@ btnAdd.addEventListener('click', function () {
 })
 
 listTodo.addEventListener("click", function (e) {
-    if (e.target.checked) {
+    if (e.target.textContent == 'x') {
+        // console.log("deletado");
+        setInterval(() => {
+            e.target.parentNode.remove();
+        }, 150)
+    }
+    else if (e.target.checked) {
         // console.log("e.target"); quem disparou o evento
         // console.log(e.target);
         // console.log("e.target.parentNode"); o elemento pai de quem disparou o evento
@@ -32,17 +39,26 @@ function createItem(task) {
     let li = document.createElement(`li`)
     li.classList.add('list__item')
 
+    //create checkbox
     let input = document.createElement('input')
     input.classList.add('input__check')
     input.setAttribute('type', 'checkbox')
 
+    //create text
     let p = document.createElement('p')
     p.classList.add('task')
     p.setAttribute('contenteditable', '')
     p.classList.add('p-2')
+    p.classList.add('w-5/6')
     p.textContent = task
+
+    //create delete button
+    let btn = document.createElement('button')
+    btn.classList.add('btn__delete')
+    btn.textContent = 'x'
 
     li.appendChild(input)
     li.appendChild(p)
+    li.appendChild(btn)
     taskList.prepend(li)
 }
